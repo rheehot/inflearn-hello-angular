4bee13cf24bcc45906ae7877eec2b9dd16f56e23
GimunLee <gimoon0226@naver.com>
2019-12-25 23:05:02 +0900

    Add timer function

--- angular/hello-angular/front-end/src/app/section/time-display/time-display.component.ts
+++ angular/hello-angular/front-end/src/app/section/time-display/time-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-time-display',
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeDisplayComponent implements OnInit {
 
-  constructor() { }
+  @Input() inputData:string;
+
+  min:number = 0; 
+  sec:number = 0;
+  ms:number = 0;
+
+  timeInterval;
+
+  constructor() { 
+  }
+
+  timeStart(){
+    this.timeInterval = setInterval(()=>{
+      this.ms++;
+    },10)
+  }
+
+  timeStop(){
+    clearInterval(this.timeInterval)
+  }
+
+  timeReset(){
+    this.timeStop();
+    this.ms=0;
+
+  }
+
+  ngOnChanges(changes:SimpleChanges){ // 변화를 감지하는 함수
+    console.log(changes)
+    for(let propName in changes){
+      if(propName == 'inputData'){
+        switch (changes[propName].currentValue){
+          case 'start':
+            this.timeStart();
+            break;
+          case 'stop':
+            this.timeStop();
+            break;
+          case 'reset':
+            this.timeReset()
+            break;
+        }
+      }
+    }
+  }
 
   ngOnInit() {
   }
+
 
 }
