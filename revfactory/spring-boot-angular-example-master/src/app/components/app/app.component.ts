ac8840ee516c75068ce93229d8624c7bd72c8646
GimunLee <gimoon0226@naver.com>
2020-01-15 15:32:59 +0900

    Fix

--- revfactory/spring-boot-angular-example-master/src/app/components/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    providers: []
-})
-export class AppComponent {
-}
--- revfactory/spring-boot-angular-example-master/src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-insert into task(title, description, version) values ('task1', 'description1', 1);
-insert into task(title, description, version) values ('두번째 작업', 'description2', 1);
